277bc3e495c9e42fef7de8ec2a4c557c5d2831a8
mtdaniels504 <matthew.daniels11@yahoo.com>
2026-07-10 11:21:24 -0400

    Update Supabase client to use service role key

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { smartIngestion } = require('./ingest.js');
 const app = express();
 app.use(express.json());
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
